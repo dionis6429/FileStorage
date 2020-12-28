e8b46249086c014a219a0cf617a847c2d4c3f169
Denis Valiuk <valukdenis@gmail.com>
2020-12-28 15:11:29 +0300

    Logger praparations.

---
+++ App/FileStorage/FileStorage.Core/Services/Interfaces/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileStorage.Core.Services.Interfaces
+{
+    public interface ILogger
+    {
+        string Log();
+    }
+}
